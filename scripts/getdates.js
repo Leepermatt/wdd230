64f12a05a16bb80b42dffb4dda86a03034322fff
Matt Leeper <leepermatt@gmail.com>
2024-01-16 11:22:31 -0800

    week 1 rev

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -2,3 +2,7 @@ const options = { weekday: 'long', day: 'numeric', month: 'long', year: 'numeric
 document.getElementById('currentdate').textContent = new Date().toLocaleDateString('en-US', options);
 
 let oLastModif = new Date(document.lastModified);
+document.getElementById('lastModified').textContent = oLastModif;
+
+let year = { year: 'numeric' };
+document.getElementById('currentYear').textContent = new Date().toLocaleDateString('en-US', year);
