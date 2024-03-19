53917d06a994b141662d2854728a4eba2c723f53
Matt Leeper <leepermatt@gmail.com>
2024-03-19 12:38:04 -0700

    w11w10weather

--- lesson10/scripts/weather.js
+++ lesson10/scripts/weather.js
@@ -2,13 +2,15 @@ const currentTemp = document.querySelector('#current-temp');
 const weatherIcon = document.querySelector('#weather-icon');
 const captionDesc = document.querySelector('figcaption');
 
-const lattitude = '6.64';
-const longitude = '-49.75';
-const apiKey = '2f78556413725d71ca9f22d69354cba8';
+const lattitude = '49.75';
+const longitude = '6.64';
+const apiKey = '2f78556413725d71ca9f22d69354cba8'
 const units = 'imperial';
-
-//const url = `https://api.openweathermap.org/data/2.5/weather?lat=6.64&lon=-49.75&units=imperial&appid=2f78556413725d71ca9f22d69354cba8`
-const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lattitude}&log=${longitude}&units=${units}&appid=${apiKey}`;
+//const url = `https://api.openweathermap.org/data/2.5/weather?lat="49.75"&lon="6.64"&appid="527e6953608f03c267561344ff6bbac1"`
+const url = `https://api.openweathermap.org/data/2.5/weather?lat=49.75&lon=6.64&units=imperial&appid=2f78556413725d71ca9f22d69354cba8`
+//const url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=${units}&appid=${apiKey}`;
+//const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lattitude}&log=${longitude}&units=${units}&appid=${apiKey}`;
+//const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lattitude}&lon=${longitude}&appid=${apiKey}`
 
 async function apiFetch() {
     try {
@@ -27,10 +29,12 @@ async function apiFetch() {
 }
 function displayResults(data) {
     currentTemp.innerHTML = `${data.main.temp}&deg;F`;
-    const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+    //const iconSrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+    const iconSrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+
     let desc = data.weather[0].description;
-    weather.setAttribute('src', iconsrc);
-    weather.setAttribute('alt', desc);
+    weatherIcon.setAttribute('src', iconSrc);
+    weatherIcon.setAttribute('alt', desc);
     captionDesc.textContent = desc;
 }
 
