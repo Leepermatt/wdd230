6f1f64c11a97c861447e5b853e08739c148d1bc6
Matt Leeper <leepermatt@gmail.com>
2024-03-19 11:27:51 -0700

    w11w9rf

---
+++ scripts/links2.js
@@ -0,0 +1,30 @@
+const baseURL = "https://leepermatt.github.io/wdd230/";
+const linksURL = "https://leepermatt.github.io/wdd230/data/links.json";
+const card = document.querySelector(".card");
+
+// Asynchronous function to get the links data from the links.json file
+async function getLinks() {
+    const response = await fetch(linksURL);
+    const data = await response.json();
+    displayLinks(data.lessons);
+}
+
+const displayLinks = (lessons) => {
+    lessons.forEach(lesson => {
+        const list = document.createElement('ul');
+        lesson.links.forEach(link => {
+            const items = document.createElement('li');
+            const linkElement = document.createElement('a');
+
+            linkElement.href = baseURL + link.url;
+            linkElement.textContent = `${lesson.lesson}: ${link.title}`
+            items.appendChild(linkElement);
+            list.appendChild(items);
+        });
+
+        card.appendChild(list);
+    });
+}
+
+// Call the getLinks() function to fetch the links data and display it
+getLinks();
