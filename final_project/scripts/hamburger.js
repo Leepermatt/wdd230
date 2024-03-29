1da5e6848d9587bb40da1671e12c024c0014e321
Matt Leeper <leepermatt@gmail.com>
2024-03-28 19:06:38 -0700

    w12fpr1

---
+++ final_project/scripts/hamburger.js
@@ -0,0 +1,12 @@
+// JavaScript to toggle visibility of navigation on click
+// document.querySelector('.hamburger').addEventListener('click', function () {
+//     const navLinks = document.querySelector('nav ul');
+//     navLinks.style.display = navLinks.style.display === 'block' ? 'none' : 'block';
+// });
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('nav');
+
+hamButton.addEventListener('click', () => {
+    navigation.classList.toggle('open');
+    hamButton.classList.toggle('open');
+});
