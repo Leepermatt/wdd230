50490c593e0db997161d5df2de3ecb51c17e30f0
Matt Leeper <leepermatt@gmail.com>
2024-04-09 14:17:12 -0700

    w14fr8

--- final_project/scripts/rentals.js
+++ final_project/scripts/rentals.js
@@ -15,19 +15,23 @@ const displayRentals = (rentals) => {
     rentals.forEach((rental) => {
         let card = document.createElement('section');
         let name = document.createElement('h2');
-        let maxPerson = document.createElement('h3')
-        let halfDay3 = document.createElement('h3')
-        let fullDay = document.createElement('h3')
+        let maxPerson = document.createElement('h3');
+        let halfDay3 = document.createElement('h3');
+        let fullDay = document.createElement('h3');
+        let halfDay6 = document.createElement('h3');
+        let fullDayExtend = document.createElement('h3');
 
         let portrait = document.createElement('img');
 
         name.textContent = `Rental Type  ${rental.RentalType}`;
         maxPerson.textContent = `Max Persons: ${rental.MaxPersons}`;
         halfDay3.textContent = `Price for half day 3 hours: ${rental.HalfDay3hrs}`;
-        fullDay.textContent = `Price for full day: ${rental.FullDay}`
-        // Build the image portrait by setting all the relevant attributes
+        fullDay.textContent = `Price for full day: ${rental.FullDay}`;
+        halfDay6.textContent = `Price for half day 6 hours: ${rental.HalfDay6hrs}`;
+        fullDayExtend.textContent = `Price for full day extended: ${rental.FullDayExtended}`;
+
         portrait.setAttribute('src', rental.Image);
-        portrait.setAttribute('alt', `Picture  of ${rental.name}`);
+        portrait.setAttribute('alt', `Picture  of ${rental.RentalType}`);
         portrait.setAttribute('loading', 'lazy');
         portrait.setAttribute('width', '340');
         portrait.setAttribute('height', '440');
@@ -36,6 +40,9 @@ const displayRentals = (rentals) => {
         card.appendChild(name); //fill in the blank
         card.appendChild(maxPerson);
         card.appendChild(halfDay3);
+        card.appendChild(fullDay);
+        card.appendChild(halfDay6);
+        card.appendChild(fullDayExtend);
 
         card.appendChild(portrait);
 
